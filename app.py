f35bb1d959772891ed3dc5f50535821580d8c25b
WhyPine <goodinga12@gmail.com>
2024-05-29 16:59:28 -0700

    Modified directory and app structure.

--- app.py
+++ app.py
@@ -2,20 +2,16 @@ from flask import Flask, jsonify, request
 import psycopg2
 import os
 
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
-DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'],
-    dbname=conn_str_params['dbname']
-)
 
-DB_USER=conn_str_params['user']
-DB_PASS=conn_str_params['password']
-DB_HOST=conn_str_params['host']
-DB_NAME=conn_str_params['dbname']
+ENV_VARS = os.environ.get("DATABASE_CONNECTION_STRING")
+PARAMS = {pair.split('=')[0]: pair.split('=')[1] for pair in ENV_VARS.split(' ')}
+print(ENV_VARS)
+
+DB_USER=PARAMS['user']
+DB_PASS=PARAMS['password']
+DB_HOST=PARAMS['host']
+DB_NAME=PARAMS['dbname']
 
 conn = psycopg2.connect(
     dbname=DB_NAME,
--- creds.py
+++
@@ -1,6 +0,0 @@
-azurePSQLID = "psqlsde342nzxc01243xzadsz231zx"
-azurePSQLKEY = "sbe3234vcbjyfgh;345!-szv324lo137/?das1"
-
-
-azureUsername = "lwfmylibec"
-azurePassword = "scSWw3w1yPuFXOS$"
